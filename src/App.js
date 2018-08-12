ea264dbfe02aee85416aa909a487fef5e49c5d30
ndhuanhuan <ndhuanhuan@gmail.com>
2018-08-12 02:04:40 -0500

    p73

--- src/App.js
+++ src/App.js
@@ -80,20 +80,18 @@ class App extends Component {
   render() {
     const {searchTerm, list} = this.state;
     return (<div className="App">
-      <Search value={searchTerm} onChange={this.onSearchChange}/>
+      <Search value={searchTerm} onChange={this.onSearchChange}>
+        Search
+      </Search>
       <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss}/>
     </div>);
   }
 }
 
-class Search extends Component {
-  render() {
-    const {value, onChange} = this.props;
-    return (<form>
-      <input type="text" value={value} onChange={onChange}/>
-    </form>);
-  }
-}
+const Search = ({value, onChange, children}) => <form>
+  {children}
+  <input type="text" value={value} onChange={onChange}/>
+</form>
 
 class Table extends Component {
   render() {
@@ -108,13 +106,22 @@ class Table extends Component {
           <span>{item.num_comments}</span>
           <span>{item.points}</span>
           <span>
-            <button onClick={() => onDismiss(item.objectID)} type="button">
+            <Button onClick={() => onDismiss(item.objectID)}>
               Dismiss
-            </button>
+            </Button>
           </span>
         </div>)
       }
     </div>);
+  }
+}
+
+class Button extends Component {
+  render() {
+    const {onClick, className = '', children} = this.props;
+    return (<button onClick={onClick} className={className} type="button">
+      {children}
+    </button>);
   }
 }
 
